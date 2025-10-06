ea8ca626f55e9cb63fb7bcdc6bd7e0ca239a4649
jorenham <jhammudoglu@gmail.com>
2025-10-07 01:12:22 +0200

    Reimplement `boost::math::hermite` in rust

--- src/math/special_functions/hermite.rs
+++ src/math/special_functions/hermite.rs
@@ -1,18 +1,5 @@
 //! boost/math/special_functions/hermite.hpp
 
-use crate::ffi;
-use core::ffi::c_uint;
-
-/// Hermite Polynomial *H<sub>n</sub>(x)*
-///
-/// Note that this is the  "physicist's" Hermite polynomial.
-///
-/// Corresponds to `boost::math::hermite(n, x)`.
-/// <https://boost.org/doc/libs/latest/libs/math/doc/html/math_toolkit/sf_poly/hermite.html>
-pub fn hermite_h(n: u32, x: f64) -> f64 {
-    unsafe { ffi::math_hermite(n as c_uint, x) }
-}
-
 /// Recurrence relation for [`hermite_h`]
 ///
 /// *H<sub>n+1</sub>(x) = 2xH<sub>n</sub>(x) - 2nH<sub>n-1</sub>(x)*
@@ -35,16 +22,42 @@ pub fn hermite_h_next(n: u32, x: f64, Hn: f64, Hn_prev: f64) -> f64 {
     2.0 * (x * Hn - (n as f64) * Hn_prev)
 }
 
+/// Hermite Polynomial *H<sub>n</sub>(x)*
+///
+/// Note that this is the  "physicist's" Hermite polynomial.
+///
+/// This is a pure rust implementation equivalent to the `boost::math::hermite(n, x)` C++ function.
+/// <https://boost.org/doc/libs/latest/libs/math/doc/html/math_toolkit/sf_poly/hermite.html>
+pub fn hermite_h(n: u32, x: f64) -> f64 {
+    // Implement Hermite polynomials via recurrence:
+    let (mut p0, mut p1) = (1.0, 2.0 * x);
+    if n == 0 {
+        p0
+    } else {
+        for c in 1..n {
+            (p0, p1) = (p1, hermite_h_next(c, x, p1, p0));
+        }
+        p1
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const RTOL: f64 = 1e-14;
+
+    fn hermite_h_ffi(n: u32, x: f64) -> f64 {
+        unsafe { crate::ffi::math_hermite(n as core::ffi::c_uint, x) }
+    }
+
     #[test]
     fn test_hermite() {
-        assert_relative_eq!(hermite_h(0, 1.0), 1.0);
-        assert_relative_eq!(hermite_h(1, 1.0), 2.0);
-        assert_relative_eq!(hermite_h(2, 1.0), 2.0);
-        assert_relative_eq!(hermite_h(3, 1.0), -4.0);
-        assert_relative_eq!(hermite_h(4, 1.0), -20.0);
+        for n in 0..20 {
+            for x in -2..=2 {
+                let x = x as f64 * 0.1;
+                assert_relative_eq!(hermite_h(n, x), hermite_h_ffi(n, x), max_relative = RTOL);
+            }
+        }
     }
 }
